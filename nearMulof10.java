2f75a2cc3c21ca29b07024b8b412378d94513965
MaheshwariS <36835393+Mathimahesh@users.noreply.github.com>
2019-05-02 12:35:42 +0530

    Create nearMulof10.java

---
+++ nearMulof10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = (n/10)*10;
+        int b = a+10;
+        System.out.println(n/10);
+    
+    }
+}
